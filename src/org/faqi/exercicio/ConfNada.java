5ad560aaf9e0999e00996c016661280db032a963
Nilseu Padilha <ragnarokkrr.blog@gmail.com>
2012-07-03 11:44:53 -0300

    Diego co piloto e Guaraci Piloto

---
+++ src/org/faqi/exercicio/ConfNada.java
@@ -0,0 +1,14 @@
+package org.faqi.exercicio;
+
+public class ConfNada {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	 	
+
+	}
+
+}
---
+++ src/org/faqi/exercicio/Nadador.java
@@ -0,0 +1,18 @@
+package org.faqi.exercicio;
+
+public class Nadador {
+
+	private String nome;
+	private int idade;
+	
+	public String getNome(){
+		return nome;
+	}
+	public int getIdade(){
+		return idade;
+	}
+	public void setNome(String nome){
+		this.nome = nome; 
+	}
+	public void setIdade( int idade){
+}
